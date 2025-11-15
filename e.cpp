adc5a0ea2c69f54740440a6482005cbb75e18bfe
Georgiy Pogrebitskiy <goshik200712@gmail.com>
2025-11-15 22:56:59 +0300

    task e, 100/100 done, smart meet-in-the-middle solution which I thought of myself!!!!

--- e.cpp
+++ e.cpp
@@ -4,7 +4,7 @@
 int* w;
 double* v;
 
-void solve_backpack(double** dp, int n, int W, int ignore) {
+void solve_forwards(double** dp, int n, int W) {
     for (int i = 0; i <= n; ++i) {
         dp[i][0] = 0;
     }
@@ -16,16 +16,40 @@ void solve_backpack(double** dp, int n, int W, int ignore) {
     for (int i = 1; i <= n; ++i) {
         int item = i - 1;
         for (int j = 1; j <= W; ++j) {
-            if (item == ignore) {
-                dp[i][j] = dp[i - 1][j];
-                continue;
-            }
-
             if (j - w[item] >= 0) {
                 dp[i][j] = std::max(dp[i - 1][j - w[item]] + v[item], dp[i - 1][j]);
             } else {
                 dp[i][j] = dp[i - 1][j];
             }
+        }
+    }
+}
+
+void solve_backwards(double** dp, int n, int W) {
+    for (int i = 0; i <= n; ++i) {
+        dp[i][0] = 0;
+    }
+
+    for (int j = 0; j <= W; ++j) {
+        dp[0][j] = 0;
+    }
+
+    for (int i = 1; i <= n; ++i) {
+        int item = n - i;
+        for (int j = 1; j <= W; ++j) {
+            if (j - w[item] >= 0) {
+                dp[i][j] = std::max(dp[i - 1][j - w[item]] + v[item], dp[i - 1][j]);
+            } else {
+                dp[i][j] = dp[i - 1][j];
+            }
+        }
+    }
+}
+
+void output(double** dp, int n, int W) {
+    for (int i = 0; i <= n; ++i) {
+        for (int j = 0; j <= W; ++j) {
+            std::cout << std::showpoint << std::fixed << std::setprecision(1) << dp[i][j] << (j == W ? "\n" : " ");
         }
     }
 }
@@ -41,25 +65,42 @@ int main(int, char**) {
         std::cin >> w[i] >> v[i];
     }
 
-    double** dp = new double*[n + 1];
+    double** dpF = new double*[n + 1];
+    double** dpB = new double*[n + 1];
     for (int i = 0; i <= n; ++i) {
-        dp[i] = new double[W + 1];
+        dpF[i] = new double[W + 1];
+        dpB[i] = new double[W + 1];
     }
 
+    solve_forwards(dpF, n, W);
+    solve_backwards(dpB, n, W);
+
     double* answer = new double[n];
-    for (int i = 0; i < n; ++i) {
-        solve_backpack(dp, n, W, i);
-        answer[i] = dp[n][W];
+    for (int ignored = 0; ignored < n; ++ignored) {
+        double maxD = 0;
+        for (int j = 0; j <= W; ++j) {
+            double d = dpF[ignored][W - j] + dpB[n - 1 - ignored][j];
+            maxD = std::max(maxD, d);
+        }
+        answer[ignored] = maxD;
     }
+
+    /*
+    output(dpF, n, W);
+    std::cout << std::endl;
+    output(dpB, n, W);
+    */
 
     for (int i = 0; i < n; ++i) {
         std::cout << std::showpoint << std::fixed << std::setprecision(9) << answer[i] << std::endl;
     }
 
     for (int i = 0; i <= n; ++i) {
-        delete dp[i];
+        delete dpF[i];
+        delete dpB[i];
     }
-    delete[] dp;
+    delete[] dpF;
+    delete[] dpB;
 
     delete[] answer;
     delete[] w;
